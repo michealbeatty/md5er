22ad49b76468c869a6a2dcddf5b63c9b56b15acd
Micheal Beatty <mike@mwbeatty.org>
2016-09-10 13:19:02 -0500

    Refactor, add write to file functionality

--- md5er.go
+++ md5er.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/hex"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,25 +11,48 @@ import (
 	"path/filepath"
 )
 
-func main() {
-	fmt.Println("This is the md5 program")
-	cwd, err := os.Getwd()
+//RecursiveHash will perform the MD5 operation recursively beginning with the
+// directory in which the application is executed.
+func recursiveHash(dir string, fout string) {
+	f, err := os.Create(fout)
 	if err != nil {
 		log.Print(err)
 	}
-	filepath.Walk(cwd, func (path string, file os.FileInfo, err error) error {
+	defer f.Close()
+	filepath.Walk(dir, func(path string, file os.FileInfo, _ error) error {
 		if !file.IsDir() {
 			fileData, err := ioutil.ReadFile(path)
 			if err != nil {
 				log.Print(err)
 			}
-			rel, err := filepath.Rel(cwd, path)
+			rel, err := filepath.Rel(dir, path)
 			if err != nil {
 				log.Print(err)
 			}
-			fmt.Printf("%x\t*%s\n", md5.Sum(fileData), rel)
+			fmt.Printf("%x *%s\n", md5.Sum(fileData), rel)
+			value := md5.Sum(fileData)
+
+			s := hex.EncodeToString(value[:]) + " *" + rel + "\n"
+			f.WriteString(s)
 			//fmt.Println(path, file.Name())
-	}
-	return nil
-})
+		}
+		return nil
+	})
+	f.Sync()
 }
+
+func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Print(err)
+	}
+	base := filepath.Base(cwd)
+	fname := flag.String("o", base+".md5", "name of output file (should have md5 extension")
+	flag.Parse()
+	recursiveHash(cwd, *fname)
+}
+
+// TODO: add flag to allow user to specify if it should be recursive or not.
+// If not recursive, should be only the contents of the current directory
+// TODO: write results to file user should be able to specify a file name or default
+// to root directory name. THis will use ioutil.WriteFile()
