d27c9ba16a0e96dc8cc28d14b7f86b0580768777
Micheal Beatty <mike@mwbeatty.org>
2016-09-09 21:04:53 -0500

    Remove commented code

--- md5er.go
+++ md5er.go
@@ -9,40 +9,6 @@ import (
 	"path/filepath"
 )
 
-// func walkpath(path string, file os.FileInfo, err error) error {
-
-// 	if !file.IsDir() {
-// 		fileData, err := ioutil.ReadFile(path)
-// 		if err != nil {
-// 			log.Print(err)
-// 		}
-// 		fmt.Printf("%s\t%x\n", path, md5.Sum(fileData))
-// 		//fmt.Println(path, file.Name())
-// 	}
-// 	return nil
-// }
-
-// func readDir(directory string) {
-
-// 	files, err := ioutil.ReadDir(directory)
-// 	fmt.Println(os.Getwd())
-// 	if err != nil {
-// 		log.Print(err)
-// 	}
-// 	for _, file := range files {
-// 		if file.IsDir() {
-// 			readDir(file.Name())
-// 		} else {
-// 			fmt.Printf("%s\t", file.Name())
-// 			fileData, err := ioutil.ReadFile(file.Name())
-// 			if err != nil {
-// 				log.Print(err)
-// 			}
-// 			fmt.Printf("%x\n", md5.Sum(fileData))
-// 		}
-// 	}
-// }
-
 func main() {
 	fmt.Println("This is the md5 program")
 	cwd, err := os.Getwd()
